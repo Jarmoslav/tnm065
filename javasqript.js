eb2c07d86390f7e03f039b72691d42ee425a91a4
Jarmoslav <simja832@student.liu.se>
2013-12-11 11:24:33 +0100

    Gjorde AJAX call för dynamsiksökning av bilder.

--- javasqript.js
+++ javasqript.js
@@ -2,41 +2,38 @@
  * @author Simon Jare
  */
 
-$(document).ready(function(){
-	$(".search").keyup(function(){
+$(document).ready(function() {
+	$(".search").keyup(function() {
 		var searchbox = $(this).val();
-		var dataString = 'searchword='+ searchbox;
-		if(searchbox==''){
-	     console.log("empty search string");
-	     window.location = "index.php"
-	     
-		}
-		else{
+		var dataString = 'searchword=' + searchbox;
+		if (searchbox == '') {
+			window.location = "index.php"
+
+		} else {
 			$.ajax({
-			type: "POST",
-			url: "search.php",
-			data: dataString,
-			dataType: "xml",
-			cache: false,
-			beforesend:function(xml){
-			$(xml).find(function(){
-					console.log(data);
-					$('.photoFrame').text(data);
+				type : "POST",
+				url : "search.php",
+				data : dataString,
+				dataType : "xml",
+				cache : false,
+				success : function(data) {
+                    $("#mainFeed").html('');
+                    var htmlString = '';
+					//get the xml info via javasqript
+					var picUrl = $(data).find('picurl').text();
+					var picUser = $(data).find('picuser').text();
+					var picTime = $(data).find('pictime').text();
 					
-			});
-           // $(xml).find("<picture>").empty();
-            
-            },
-			success: function(data){
-			   $xml = $(data); // gör om till Jqury object. 
-			   $title = $xml.find( "picture" );
-			   $( "#mainFeed" ).append( $title);
-			   console.log(data);
-					
-					
-		
+					htmlString += '<div class = "photoFrame">';
+					htmlString += '<a href ="viewImage.php?pictureID="' + picUrl + '><img height = "120" src = ' + picUrl + ' alt = "test"/></a>';
+					htmlString += '<p>Uploaded by:<a class = "userlink" href="#">' + picUser + ' </a></p>';
+					htmlString += '<p class = "pictime">' + picTime + '</p>';
+					htmlString += '</div>';
+					//set the values into the DOM
+					$("#mainFeed").append(htmlString);
 				}
 			});
-		}return false;
+		}
+		return false;
 	});
-});
+}); 
